d271a218edeba0e5bbd90de288d948f78fffc581
arindam-chattopadhyay-3103 <arindam3103@gmail.com>
2025-05-03 18:00:47 +0530

    Commit 04

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.firstapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
 
+    private val textView = findViewById<TextView>(R.id.helloTextView)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.text = getString(R.string.my_documents)
+        textView.contentDescription = getString(R.string.my_documents) + "content"
     }
 
 }
