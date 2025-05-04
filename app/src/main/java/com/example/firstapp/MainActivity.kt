4a5445ba5bdb6441b33a8d8a0cf29a88d531f69b
arindam-chattopadhyay-3103 <arindam3103@gmail.com>
2025-05-04 20:27:59 +0530

    Commit 06

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,17 +1,40 @@
 package com.example.firstapp
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
 
+    lateinit var buttonWhatsApp: Button
+    lateinit var uriString: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView = findViewById<TextView>(R.id.helloTextView)
-        textView.text = getString(R.string.my_documents)
-        textView.contentDescription = getString(R.string.my_documents) + "content"
+        uriString = "Go to link : https://www.youtube.com/watch?v=W8MjnsJ820E"
+        buttonWhatsApp = findViewById<Button>(R.id.whatsapp_button)
+        buttonWhatsApp.setOnClickListener {
+            whatsappShare(uriString)
+        }
+    }
+
+    fun whatsappShare(uriString: String) {
+        try {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, uriString)
+            intent.setPackage("com.whatsapp")
+            startActivity(intent)
+        } catch (exception: Exception) {
+            Toast.makeText(
+                applicationContext,
+                "$exception :: Whatsapp not installed",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
 }
